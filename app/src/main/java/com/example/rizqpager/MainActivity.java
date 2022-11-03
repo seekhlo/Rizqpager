b4403de9e299acee28c6ba0e12044e5968001931
Usama Ali <seekhlo7224@gmail.com>
2022-11-03 05:21:59 -0700

    three pages

--- app/src/main/java/com/example/rizqpager/MainActivity.java
+++ app/src/main/java/com/example/rizqpager/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.rizqpager;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
+    LinearLayout sliderDotspanel;
+    private int dotscount;
+    private Button getStartedBtn;
+    private ImageView[] dots;
     ArrayList<Integer> arrayList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewPager = findViewById(R.id.viewpager1);
@@ -17,6 +30,51 @@ public class MainActivity extends AppCompatActivity {
         arrayList.add(R.drawable.third);
         MyAdapter myAdapter = new MyAdapter(MainActivity.this, arrayList);
         viewPager.setAdapter(myAdapter);
+        sliderDotspanel = findViewById(R.id.sliderdots);
+        dotscount = myAdapter.getCount();
+        dots = new ImageView[dotscount];
+        getStartedBtn = findViewById(R.id.getstarted);
+
+        for(int i = 0; i < dotscount; i++){
+
+
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.not_active_dot));
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(8, 0, 8, 0);
+            sliderDotspanel.addView(dots[i], params);
+        }
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                for(int i = 0; i< dotscount; i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.not_active_dot));
+                }
+
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        getStartedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), phoneNumber.class );
+                startActivity(i);
+            }
+        });
     }
 }
 
---
+++ app/src/main/java/com/example/rizqpager/otp.java
@@ -0,0 +1,116 @@
+package com.example.rizqpager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.poovam.pinedittextfield.LinePinField;
+
+public class otp extends AppCompatActivity {
+    ImageView back;
+
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btnback;
+    String a;
+    LinePinField inputfield;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+        back= findViewById(R.id.arrow);
+        btn1= findViewById(R.id.button1);
+        btn2= findViewById(R.id.button2);
+        btn3= findViewById(R.id.button3);
+        btn4= findViewById(R.id.button4);
+        btn5= findViewById(R.id.button5);
+        btn6= findViewById(R.id.button6);
+        btn7= findViewById(R.id.button7);
+        btn8= findViewById(R.id.button8);
+        btn9= findViewById(R.id.button9);
+        btn0= findViewById(R.id.button0);
+        btnback = findViewById(R.id.buttonback);
+        inputfield = findViewById(R.id.otp);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a = inputfield.getText().toString();
+                inputfield.setText(a+"1");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a = inputfield.getText().toString();
+                inputfield.setText(a+"2");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a = inputfield.getText().toString();
+                inputfield.setText(a+"3");
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a = inputfield.getText().toString();
+                inputfield.setText(a+"4");
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a = inputfield.getText().toString();
+                inputfield.setText(a+"5");
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a = inputfield.getText().toString();
+                inputfield.setText(a+"6");
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a = inputfield.getText().toString();
+                inputfield.setText(a+"7");
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a = inputfield.getText().toString();
+                inputfield.setText(a+"8");
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a = inputfield.getText().toString();
+                inputfield.setText(a+"9");
+            }
+        });
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a = inputfield.getText().toString();
+                inputfield.setText(a+"0");
+            }
+        });
+
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            a = inputfield.getText().toString();
+            a = a.substring(0, a.length()-1);
+            inputfield.setText(a);
+            }
+        });
+    }
+}
